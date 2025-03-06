cb887a190ad92b00478d9b0a116d8ecd154b08d2
1Kaustubh122 <kkaustubh2006@gmail.com>
2025-03-06 07:02:58 +0530

    added visualization

--- Monte_Carlo/first_visit_MC.py
+++ Monte_Carlo/first_visit_MC.py
@@ -1,4 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import random
+import os
+
+os.makedirs("Monte_Carlo/Results", exist_ok=True)
 
 class FirstVisitMcPredection:
     def __init__(self, pi=None, gamma=0.9):
@@ -13,6 +18,8 @@ class FirstVisitMcPredection:
             (1, 0): "up",    (1, 1): "up",    (1, 2): "right",
             (2, 0): "up",    (2, 1): "right", (2, 2): "up"
         }
+        
+        self.mse_lst = [] 
 
         
 
@@ -58,28 +65,59 @@ class FirstVisitMcPredection:
         
         G = 0
         visited_states = set()
+        alpha = 0.01
         
         for state, action, reward in epsiode_lst:
             G = self.gamma * G + reward
             
             if state not in visited_states and state not in self.rewards:
                 self.num_visits[state] += 1
-                self.val_pi_s[state] += (G - self.val_pi_s[state]) / self.num_visits[state]
+                self.val_pi_s[state] += alpha * (G - self.val_pi_s[state]) / self.num_visits[state]
                 visited_states.add(state)
                 
             
-    def train(self, num_episodes=1000):
+    def train(self, num_episodes=200):
+        
         for i in range(num_episodes):
-            st_state = self.state_space[np.random.randint(len(self.state_space))]
+            prev_val_pi_s = self.val_pi_s.copy()
+            st_state = random.choice([s for s in self.state_space if s not in self.rewards]) 
             self.episode_gen(st_state)
             self.monte_carlo()
+            
+            mse = np.mean([abs(self.val_pi_s[s] - prev_val_pi_s[s]) ** 2 for s in self.state_space])
+            self.mse_lst.append(mse)
 
-            if i % 100 == 0:
+            if i % 10 == 0:
                 print(f"Iteration {i}, Values:")
                 for row in range(3):
                     print([round(self.val_pi_s[(row, col)], 2) for col in range(3)])
                 print("\n")
+    
+    def plot_res(self):
+        plt.plot(self.mse_lst, label="MSE")
+        plt.xlabel("Episodes")
+        plt.ylabel("Mean Squared Error")
+        plt.title("MSE of Value Function")
+        plt.grid()
+        plt.savefig("Monte_Carlo/Results/first_visit_mc_mse.png") 
+        plt.show()
+        
+        val_mat = np.zeros((3,3)) 
+        for (i, j), val in self.val_pi_s.items():
+            val_mat[i, j] = val
+            
+        plt.imshow(val_mat, cmap="coolwarm",interpolation="nearest")
+        
+        for i in range(3):
+            for j in range(3):
+                plt.text(j, i, round(val_mat[i, j], 2), ha="center", va="center")
+                
+        plt.colorbar(label="State Value")
+        plt.title("Final Value Function Heatmap")
+        plt.savefig("Monte_Carlo/Results/first_visit_mc_heat_map.png") 
+        plt.show()
 
 
 alpha = FirstVisitMcPredection()
 alpha.train()
+alpha.plot_res()
