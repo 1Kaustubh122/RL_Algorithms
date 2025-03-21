0c55bb0fe88802331e88e322e745d2758d9b5c82
1Kaustubh122 <kkaustubh2006@gmail.com>
2025-03-21 07:40:17 +0530

    Updated policy

--- utils/policy.py
+++ utils/policy.py
@@ -1,11 +1,49 @@
 import numpy as np
 
 class PolicySelection:
-    def __init__(self, action_space, epsilon =0.9):
+    def __init__(self, action_space, policy_type, epsilon =0.9):
         self.action_space= action_space
         self.epsilon = epsilon
-        self.epsilon_policy = {}
+        self.policy_type = policy_type
+        # self.epsilon_policy = {}
+    
+    def select_action(self, state, policy_type, Q=None):
+        if policy_type == "random":
+            return np.random.choice(self.action_space)
         
+        elif policy_type == "fixed_action":
+            return 0
+        
+        elif Q is not None:
+            if policy_type == "e-greedy":
+                return self.e_greedy_policy(Q[state])
+            
+            elif policy_type == "off_policy_behavior":
+                return self.behavior_policy(Q[state])
+            
+            elif policy_type == "greedy":
+                return self.greedy_sel(Q[state])
+            
+            elif policy_type == "off_policy_target":
+                return self.target_policy(Q[state])
+            
+            else:
+                raise ValueError("Policy not Found")
+                
+        
+        else:
+            raise ValueError("Either Q-Value is None or policy not Found")
+            
+    def get_policy_probab_e_greedy(self, state, action, policy_type, Q=None):
+        num_actions = len(self.action_space)
+        
+        best_action = np.argmax(Q[state])
+        if action == best_action:
+            return 1 - self.epsilon
+        else:
+            return self.epsilon/ num_actions
+
+
     def greedy_sel(self, Q_action_value_table):
         """ Returns the action with the highest Q Values (greedy selection)"""
         max_value = np.max(Q_action_value_table)
@@ -33,13 +71,5 @@ class PolicySelection:
         """ Returns the greedy action, used as the target policy in off-policy learning"""
         return np.argmax(Q_action_value_table)
     
-    def epsilon_greedy_probab(self, Q_value):
-        num_actions = len(self.action_space)
-        best_action = np.argmax(Q_value)
-
-        action_probabs = np.ones(num_actions) * (self.epsilon/ num_actions)
-        action_probabs[best_action] += (1-self.epsilon)
-
-        return action_probabs
 
 
